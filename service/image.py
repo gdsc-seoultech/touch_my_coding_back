93073a2f1ae68e1be212685e356eca18b1eac6aa
juijeong8324 <juijeong8324@gmail.com>
2022-03-25 20:33:28 +0900

    이미지 기능 키워드 추가

--- service/image.py
+++ service/image.py
@@ -1,8 +1,14 @@
+from ast import keyword
+from urllib import response
+from flask import jsonify
 import requests
+import json
+from google.cloud import vision # Imports the Google Cloud client library
+from google.cloud import storage
+
 
 class Image:
     def searchImage(query, page):
-        
         image_url_list = []
 
         requset_url = 'https://unsplash.com/napi/search' # 웹 요청 
@@ -30,4 +36,18 @@ class Image:
         
         return server_response
 
-    
+    def keyWord(uri):
+       # Instantiates a client
+        client = vision.ImageAnnotatorClient()
+
+        image = vision.Image()
+        image.source.image_uri = uri 
+
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations 
+
+        for label in labels:
+            print(label.description) # 키워드만 출력
+
+
